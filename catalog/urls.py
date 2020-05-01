11b6f8346d8cd3b1f47fe4f581ed73552c0879d3
KTagle <kyletagle727@gmail.com>
2020-05-01 16:59:13 +0800

    borrow and return works

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 	path('', views.home, name= 'home'),
 	path('books', views.books, name = 'books'),
-	path('author', views.authors),
-	path('books/<str:pk>/', views.booksID, name= 'instance')
+	path('books/<str:pk>/', views.booksID, name= 'instance'),
+	path('borrow/<str:pk>/', views.reserveBook, name='reserve'),
+	path('return/<str:pk>/', views.returnBook, name='ret')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import F
 
 from .models import *
 # Create your views here.
 
-def home(request):
+def home(request, context=None):
     borrow = BookInstance.objects.filter(status='r')
     return render(request, 'catalog/dashboard.html', {'borrow':borrow})
 
@@ -12,8 +13,6 @@ def books(request):
     books = Book.objects.all()
     return render(request, 'catalog/books.html', {'books': books})
     
-def authors(request):
-    return render(request, 'catalog/authors.html')
 
 def booksID(request, pk):
     book = Book.objects.get(id=pk)
@@ -22,3 +21,20 @@ def booksID(request, pk):
 
     context = {'book':book, 'instance':instance}
     return render(request, 'catalog/bookinstance.html', context)
+
+def reserveBook(request, pk):
+    reserve = BookInstance.objects.get(id=str(pk))
+
+    reserve.status = 'r'
+    reserve.save()
+
+    return home(request, {'reserve':reserve})
+
+def returnBook(request, pk):
+    ret = BookInstance.objects.get(id=str(pk))
+
+    ret.status = 'a'
+    ret.due_back = None
+    ret.save()
+
+    return home(request, {'ret':ret})
